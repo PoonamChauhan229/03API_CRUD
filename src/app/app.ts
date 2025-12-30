4a1c84df4746a92a429d212524525e7d9c96c72c
Poonam Chauhan <68181154+Peecee596@users.noreply.github.com>
2025-12-31 03:48:26 +0530

    started

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
+import { Register } from './register/register';
+import { Navbar } from './navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Login,Register,Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
